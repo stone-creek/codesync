f2f0279d2e4324f66b5cd540f1545364fb8ee588
John Steinbach <jfernandosimoes@gmail.com>
2026-01-19 00:01:59 +0000

    Ghost fix

--- stonecreek-mockups.lua
+++ stonecreek-mockups.lua
@@ -16,10 +16,28 @@ Players.Count = function()
 end
 Players.EquipWeapon = function(player, weapon_info)
 end
+Players.FlashMessage = function(player, message)
+end
+Players.GetCoords = function(player)
+    return {
+        x = 1,
+        y = 2,
+        z = 3
+    }
+end
 -------------------------------------------------------------------------------
 Inventory = {}
 Inventory.RemoveFrom = function(player,inventory)
     return true
+end
+Inventory.Count = function(player)
+    return 1
+end
+Inventory.At = function(player,pos)
+    return "random item guid" 
+end
+Inventory.ClassOf = function(player,item)
+    return "random item class" 
 end
 -------------------------------------------------------------------------------
 Effects = {}
@@ -46,5 +64,11 @@ WorldItems.AddMeshTo = function(item,mesh,x,y,z,rotation)
     return "spawned-item-guid-0001"
 end
 WorldItems.Set = function(object,variable,table)
+end
+WorldItems.NearestAt = function(x,y,z)
+    return "nearest-item-at"
+end
+WorldItems.ClassOf = function(object)
+    return "class-of-item"
 end
 -------------------------------------------------------------------------------
